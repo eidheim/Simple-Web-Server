cc838b6ec235d98247384d5a41b10921e558df79
eidheim <eidheim@gmail.com>
2017-08-13 00:07:45 +0200

    Minor utility.hpp cleanup

--- utility.hpp
+++ utility.hpp
@@ -104,8 +104,8 @@ namespace SimpleWeb {
         return result;
 
       size_t name_pos = 0;
-      size_t name_end_pos = static_cast<size_t>(-1);
-      size_t value_pos = static_cast<size_t>(-1);
+      auto name_end_pos = std::string::npos;
+      auto value_pos = std::string::npos;
       for(size_t c = 0; c < query_string.size(); ++c) {
         if(query_string[c] == '&') {
           auto name = query_string.substr(name_pos, (name_end_pos == std::string::npos ? c : name_end_pos) - name_pos);
@@ -114,8 +114,8 @@ namespace SimpleWeb {
             result.emplace(std::move(name), Percent::decode(value));
           }
           name_pos = c + 1;
-          name_end_pos = static_cast<size_t>(-1);
-          value_pos = static_cast<size_t>(-1);
+          name_end_pos = std::string::npos;
+          value_pos = std::string::npos;
         }
         else if(query_string[c] == '=') {
           name_end_pos = c;
